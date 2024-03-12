35e2a079445b9d64616de780506e65ae1c37fc14
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-03-12 20:18:44 +0900

    게시글 쓰기

---
+++ src/main/java/shop/mtcoding/blog/board/Board.java
@@ -0,0 +1,19 @@
+package shop.mtcoding.blog.board;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+@Entity
+@Table(name = "board_tb")
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String content;
+    private String username;
+    private Timestamp createdAt;
+}
